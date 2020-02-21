0bac0550e0f4a627fcd0223da75f77bda97e7510
Frankie Claessens <claessensfrankie@gmail.com>
2020-02-21 08:27:15 +0100

    Added credits to unsplash

--- src/unsplash.jsx
+++ src/unsplash.jsx
@@ -5,7 +5,7 @@ let random = 0;
 export const headers = new Headers({
     'Authorization': `Client-ID ${unsplashConfig.accessKey}`
 });
-export const command = config.unsplash.enabled ? async (dispatch) => await getCollection(): '';
+export const command = config.unsplash.enabled ? async (dispatch) => await getCollection() : '';
 
 const getCollection = async () => {
     return await (await fetch(`https://api.unsplash.com/collections/${unsplashConfig.collectionId}/photos?per_page=${unsplashConfig.perPage}`, { headers: headers })).json()
@@ -16,26 +16,33 @@ const randomIntFromInterval = (max) => {
 };
 
 export const render = ({ output }) => {
-    if(config.unsplash.enabled) {
+    if (config.unsplash.enabled) {
         random = window.navigator.onLine && output ? randomIntFromInterval(output.length) : 0;
-    return (
-        window.navigator.onLine ? (
-            unsplashConfig ? (
-                output && output.length > random ? (
-                    <div className="unsplash">
-                        <img alt="unsplash" className="image" src={output[random].urls.full} />
-                    </div>
-                ) : 'error'
+        return (
+            window.navigator.onLine ? (
+                unsplashConfig ? (
+                    output && output.length > random ? (
+                        <div className="unsplash">
+                            <img alt="unsplash" className="image" src={output[random].urls.full} />
+                            <div className="credits">
+                                <div className="link">
+                                    <span className="description">{output[random]['alt_description'] || ''}</span>
+                                    <span className="by">&nbsp;by&nbsp;</span>
+                                    <span className="username">{output[random].user.username}</span>
+                                </div>
+                            </div>
+                        </div>
+                    ) : 'error'
+                ) : (
+                        <div className="error">
+                            <i className="icon far fa-exclamation-triangle" />
+                        </div>
+                    )
             ) : (
-                    <div className="error">
-                        <i className="icon far fa-exclamation-triangle" />
-                    </div>
+                    ''
                 )
-        ) : (
-                ''
-            )
 
-    )
+        )
     }
     document.getElementById('unsplash-jsx').classList.add('none');
     return ''
